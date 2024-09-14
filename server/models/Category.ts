69a328615d7de9c116e5692a9f4aa52c4825a99c
AayushBharti <iaayushbharti@gmail.com>
2024-09-14 11:05:22 +0530

    Added Category model

---
+++ server/models/Category.ts
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+// Define the Tags schema
+const categorySchema = new mongoose.Schema({
+	name: {
+		type: String,
+		required: true,
+	},
+	description: { type: String },
+	courses: [
+		{
+			type: mongoose.Schema.Types.ObjectId,
+			ref: "Course",
+		},
+	],
+});
+
+// Export the Tags model
+export default mongoose.model("Category", categorySchema);
