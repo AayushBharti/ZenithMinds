8084189b473916c7c882cc915eb990abafbd9440
AayushBharti <iaayushbharti@gmail.com>
2024-09-21 10:49:30 +0530

    Added types.d.ts

---
+++ server/types.d.ts
@@ -0,0 +1,22 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import { Request } from "express"
+import { UploadedFile } from "express-fileupload"
+
+// export {}
+
+// Extend the Express Request interface
+declare global {
+  namespace Express {
+    interface Request {
+      user: {
+        id: string
+        email: string
+        accountType: string
+      }
+      files?: {
+        thumbnailImage?: UploadedFile
+        videoFile?: UploadedFile
+      }
+    }
+  }
+}
