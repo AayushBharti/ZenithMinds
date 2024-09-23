f25111960d38dea17f02b094baeffabe87ab30c9
AayushBharti <iaayushbharti@gmail.com>
2024-09-23 18:16:11 +0530

    updated configurations

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'raw.githubusercontent.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+    deviceSizes: [320, 420, 768, 1024, 1200],
+    imageSizes: [16, 32, 48, 64, 96],
+    formats: ["image/avif", "image/webp"],
+  },
+}
+export default nextConfig
