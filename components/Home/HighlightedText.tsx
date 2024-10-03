058ec6c7c996218f54e242c2d323678c99bde3fd
AayushBharti <iaayushbharti@gmail.com>
2024-10-03 14:21:25 +0530

    updated css

--- components/Home/HighlightedText.tsx
+++ components/Home/HighlightedText.tsx
@@ -2,7 +2,7 @@ import React from "react"
 
 export default function HighlightText({ text }: { text: string }) {
   return (
-    <span className="bg-gradient-to-b from-[#1FA2FF] via-[#12D8FA] to-[#A6FFCB] text-transparent bg-clip-text font-bold">
+    <span className="bg-gradient-to-br from-[#06b7f9] to-[#194aec] max-w-fit text-transparent bg-clip-text font-bold">
       {" "}
       {text}
     </span>
