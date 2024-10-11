cf0d3df3b27da17c9ab41d32767d532ab8b32a49
aayushbharti <iaayushbharti@gmail.com>
2024-10-11 17:09:38 +0530

    added auth layout group and added login, signup component

---
+++ app/(auth)/signup/page.tsx
@@ -0,0 +1,27 @@
+"use client"
+import Template from "@/components/Auth/AuthTemplate"
+import { useAuthStore } from "@/store/useAuthStore"
+import OpenRoute from "../../../components/Auth/OpenRoute"
+
+function Signup() {
+  const { loading } = useAuthStore()
+  return (
+    <OpenRoute>
+      {loading ? (
+        <div className=" h-[100vh] flex justify-center items-center">
+          <div className="custom-loader"></div>
+        </div>
+      ) : (
+        <Template
+          title="Join the millions learning to code with ZenithMinds for free"
+          description1="Build skills for today, tomorrow, and beyond."
+          description2="Education to future-proof your career."
+          image="/Images/signup.webp"
+          formType="signup"
+        />
+      )}
+    </OpenRoute>
+  )
+}
+
+export default Signup
